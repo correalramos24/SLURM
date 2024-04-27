8a5adddad936eb322fa82585adf6fd9b3ca37341
Victor Correal <victor.correal@bsc.es>
2024-04-27 17:42:53 +0200

    Create utils_lumi.sh

---
+++ utils/utils_lumi.sh
@@ -0,0 +1,15 @@
+
+# queue commands:
+alias queue='watch -t -n 1 squeue'
+
+
+# CATS Project:
+alias sbatch_cats='sbatch --account=project_465000454'
+alias srun_cats='srun --account=project_465000454'
+alias interactive_cats='srun --account=project_465000454 --pty bash'
+
+# DEODE Project:
+alias sbatch_deode='sbatch --account=project_465000527'
+alias srun_deode='srun --account=project_465000527'
+alias interactive_cats='srun --account=project_465000527 --pty bash'
+
